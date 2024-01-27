071b57efe8cb1619b780cc5e546a5a9f4d0d7dc7
Loay Al-Said <l.programming555@gmail.com>
2024-01-27 13:26:51 +0200

    0-what-is-my-pid

---
+++ 0x05-processes_and_signals/makes_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# make an executable script checking for the sytesl of it
+
+if [ $# -ne 1 ]; then
+	echo "Usage: <./make_script.h> <script name>"
+	exit
+fi
+
+
+echo "#!/usr/bin/env bash" > "$1"
+vi "$1"
+chmod +x "$1"
+shellcheck "$1"
+./$1
