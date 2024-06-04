0c3c406abc0703e7791aad9f28fcab90e9d10307
Loay Al-Said <l.programming555@gmail.com>
2024-06-04 05:36:24 +0300

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""fetches information from JSONplaceholder API and exports to CSV"""
-
-
-import requests
+"""Use an api"""
 import json
+import requests
 import sys
-try:
-    user_id = sys.argv[1]
-except Exception:
-    exit()
 
-tasks = requests.get(
-    f"https://jsonplaceholder.typicode.com/todos/{user_id}")
-tasks = json.loads(tasks.text)
-print(tasks)
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    user_name = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}"
+        ).json().get('username')
+
+    tasks = requests.get(
+                f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    ).json()
+
+    result = []
+    for task in tasks:
+        r = {}
+        r['task'] = task['title']
+        r['completed'] = task['completed']
+        r['username'] = user_name
+        result.append(r)
+
+    final_result = {f"{user_id}": result}
+    with open(f"{user_id}.json", "w") as f:
+        json.dump(final_result, f)
